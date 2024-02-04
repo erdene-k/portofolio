39749e24ce3d74753a5b692027e8cdac2d120b1d
Khuchitchuluun Kh <kh.xvchee@gmail.com>
2024-02-05 01:03:37 +0800

    init react project

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HTML from '@react-three/drei'
+const Loader = () => {
+  return (
+<HTML>
+<div className='flex justify-center items-center'>
+        <div className="w-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+</HTML>
+  )
+}
+
+export default Loader
