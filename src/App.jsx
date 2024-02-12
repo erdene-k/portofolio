7dd1a4968903fa6b874fec2ea80b372d7be7bfe9
Khuchitchuluun Kh <kh.xvchee@gmail.com>
2024-02-12 21:35:34 +0800

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 function App() {
 
   return (
-    <main className="bg-slate-300/20">
+    <main className="bg-slate-100">
       <Router>
         <Navbar/>
         <Routes>
