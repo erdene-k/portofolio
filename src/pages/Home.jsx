48c8bae2f8a64d6587c529f63ac05f5354780f84
erd <erdenechuluun.kh@tavanbogd.com>
2024-02-15 17:27:53 +0800

    css fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,7 +33,12 @@ const Home = () => {
 
   return (
     <section className="home-page">
+
+
+    <h1>Kami sama</h1>
+   
       <div className="canvas-container">
+  
         <Suspense fallback={<Loader />}>
           <Canvas camera={{ near: 0.1, far: 1000 }}>
             <directionalLight position={[1, 1, 1]} intensity={2} />
@@ -67,8 +72,14 @@ const Home = () => {
               className="isometric"
             />
           </Canvas>
-        </Suspense>
+          <div className="info-text">
+          <p>Lorem ipsum dolor sit Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam perspiciatis veritatis commodi ducimus quaerat qui quas? Consectetur harum ipsam excepturi qui voluptatibus accusamus, repellendus fugit! Dolorum doloribus omnis possimus laboriosam. amet consecte Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam eveniet nam fuga reiciendis, harum ab quis atque neque quibusdam veniam beatae porro incidunt nihil cupiditate explicabo earum quia! Sapiente, non!tur adipisic Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus amet fugiat ratione beatae illum quam reiciendis architecto atque dignissimos. Est autem, voluptatem amet necessitatibus aut excepturi odio odit distinctio earum!ing elit. Officiis repellendus totam, quaerat sit corporis a culpa eveniet sed ex voluptate quasi explicabo cupiditate incidunt nesciunt vitae. Numquam reprehenderit debitis sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis culpa minus numquam! Unde molestias, impedit voluptate cumque, fugit possimus laborum quidem voluptatem, repellat alias perspiciatis illo tempore dolore explicabo nemo.</p>
+       
+          </div>
+   </Suspense>
+ 
       </div>
+
     </section>
   );
 };
